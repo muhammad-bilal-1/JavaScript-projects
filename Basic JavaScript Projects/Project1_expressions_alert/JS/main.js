a07c7880d77f77c1d2d45227a68beb32fb3a30f0
muhammad-bilal-1 <jutt.muhammadbilal@gmail.com>
2026-08-11 15:43:18 -0700

    project1_expression_alert
    created alerts and document write using js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+window.alert('Hello World!');//Utilize the window.alert() and document.write() methods
+
+var Sent1='This is the beginning of the string', Sent2= ' and this is the end of the string'; // Create 2 variables
+
+document.write(Sent1+Sent2);// Concatenate the Sent1 and Sent2 variables together and utilized the document.write to show the result.
+
+document.write(3+3);// write an expression
